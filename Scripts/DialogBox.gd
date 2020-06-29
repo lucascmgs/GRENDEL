5aa79c9f0538bdd6d252b136b4256b2ed6b81e56
Lucas <lucascmgsds@gmail.com>
2020-06-29 03:07:56 -0300

    Mais sprites, textbox quase pronta

---
+++ Scripts/DialogBox.gd
@@ -0,0 +1,23 @@
+extends RichTextLabel
+
+
+var dialogList = ["Primeiro", "Segundo", "Terceiro", "Fim"]
+
+var page = 0
+func _ready():
+	set_bbcode(dialogList[page])
+	set_visible_characters(0)
+	set_process_input(true)
+
+func _input(event):
+	if event.is_action_pressed("confirm_text") :
+		if get_visible_characters() > get_total_character_count() :
+			if page < dialogList.size() - 1 :
+				page += 1
+				set_bbcode(dialogList[page])
+				set_visible_characters(0)
+		else :
+			set_visible_characters(get_total_character_count())
+
+func _on_DialogTimer_timeout():
+	set_visible_characters(get_visible_characters() + 1)
